0ecf34fc388dcc8d57df454c33e7adc396e4fd53
Juan1733 <juan.araque@correo.unimet.edu.ve>
2022-08-30 15:04:18 +0000

    fixed

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -3,25 +3,25 @@ import { FaBars } from "react-icons/fa";
 
 export const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg fixed-top">
-            <div class="container-fluid">
-                <a class="navbar-brand text-light" href="#">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon text-light"><FaBars /></span>
+        <nav className="navbar navbar-expand-lg fixed-top">
+            <div className="container-fluid">
+                <a className="navbar-brand text-light" href="#">Start Bootstrap</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon text-light"><FaBars /></span>
                 </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Services</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Services</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link">Contact</a>
+                        <li className="nav-item">
+                            <a className="nav-link">Contact</a>
                         </li>
                     </ul>
                 </div>
